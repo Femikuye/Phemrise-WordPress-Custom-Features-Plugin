b97861bfc822b180e73adfd45bfddb29f08b1f75
unknown <oluwafemiolayinka35@gmail.com>
2025-06-08 22:56:15 +0100

    first commit

---
+++ Phemrise-WP-Custom-Features.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package PhemriseWpCf
+ */
+/*
+ Plugin Name: Phemrise WP Custom Features
+ Plugin URI: https://phemrise.com
+ Description: This plugin is designed to add custom features to WordPress websites
+ Author: Femi Kuye
+ Version: 1.0.0
+ Author URI: https://phemrise.com
+ License: GPLv2 or later
+ Text Domain: Phemrise
+ */
+/*
+
+*/
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
